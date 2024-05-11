7c370acb37b6363173ca8ec4bea3afc5869a908e
nyagami <hoangquan05112002@gmail.com>
2024-05-11 11:30:18 +0700

    init project

--- src/configs/general.ts
+++ src/configs/general.ts
@@ -2,20 +2,20 @@ import { env } from '@/env'
 
 export const generalConfig: GeneralConfigType = {
 
-	name: 'tscord', // the name of your bot
+	name: 'LNReader Bot', // the name of your bot
 	description: '', // the description of your bot
 	defaultLocale: 'en', // default language of the bot, must be a valid locale
 	ownerId: env.BOT_OWNER_ID,
 	timezone: 'Europe/Paris', // default TimeZone to well format and localize dates (logs, stats, etc)
 
-	simpleCommandsPrefix: '!', // default prefix for simple command messages (old way to do commands on discord)
+	simpleCommandsPrefix: '.', // default prefix for simple command messages (old way to do commands on discord)
 	automaticDeferring: true, // enable or not the automatic deferring of the replies of the bot on the command interactions
 
 	// useful links
 	links: {
-		invite: 'https://www.change_invite_link_here.com',
-		supportServer: 'https://discord.com/your_invitation_link',
-		gitRemoteRepo: 'https://github.com/barthofu/tscord',
+		invite: 'https://discord.com/oauth2/authorize?client_id=1238700375956131841&permissions=412317182016&scope=bot',
+		supportServer: 'https://discord.gg/QdcWN4MD63',
+		gitRemoteRepo: 'https://github.com/LNReader/lnreader',
 	},
 
 	automaticUploadImagesToImgur: false, // enable or not the automatic assets upload
@@ -25,12 +25,12 @@ export const generalConfig: GeneralConfigType = {
 	// define the bot activities (phrases under its name). Types can be: PLAYING, LISTENING, WATCHING, STREAMING
 	activities: [
 		{
-			text: 'discord.js v14 with tscord',
+			text: 'LNReader is reading Youzitsu year 2 vol 5',
 			type: 'PLAYING',
 		},
 		{
-			text: 'some knowledge',
-			type: 'STREAMING',
+			text: 'LNReader is using TTS',
+			type: 'LISTENING',
 		},
 	],
 
